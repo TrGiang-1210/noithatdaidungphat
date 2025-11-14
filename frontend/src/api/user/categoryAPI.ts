8acf6f56bb2c163f711f71b2d1e3212c534adc1c
TrGiang-1210 <kobiet632@gmail.com>
2025-11-14 18:39:41 +0700

    Đã có layout header và footer tạm

---
+++ frontend/src/api/user/categoryAPI.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+// Kiểu dữ liệu danh mục
+export interface Category {
+  _id: string;
+  slug: string;
+  name: string;
+  img_url?: string;
+  parent?: string; // ObjectId (dưới dạng string)
+}
+
+// Lấy danh sách tất cả danh mục
+export const fetchAllCategories = async (): Promise<Category[]> => {
+  const response = await axios.get('http://localhost:5000/api/categories');
+  return response.data;
+};
